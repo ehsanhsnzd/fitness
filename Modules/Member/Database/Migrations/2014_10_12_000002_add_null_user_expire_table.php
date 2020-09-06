bb1bb354fc69b8a042eb000ee2bcb1079c6695a0
Ehsan <ehsan.hsnzd@gmail.com>
2020-09-06 16:49:37 +0430

    fix user expire to null

---
+++ Modules/Member/Database/Migrations/2014_10_12_000002_add_null_user_expire_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddNullUserExpireTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('expire_date');
+            $table->timestamp('expire_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('expire_date');
+            $table->timestamp('expire_date');
+        });
+    }
+}
